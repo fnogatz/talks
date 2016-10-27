3431fc32c99b6548810a0db563cd8b88c057a352
Falco Nogatz <fnogatz@gmail.com>
2016-10-27 23:30:15 +0200

    Praktikum WS 2016

---
+++ ws16-uniwue-praktikum/slide_config.js
@@ -0,0 +1,33 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Datenbanken und<br>Regelbasierte Systeme',
+    subtitle: 'Praktikum im Wintersemester 2016',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/uulm.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['default', 'uniwue-changes'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Falco Nogatz',
+    company: 'Lehrstuhl für Informatik I',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@ulmerleben',
+    www: 'http://www.you.com',
+    github: 'http://github.com/fnogatz'
+  }]
+};
+
