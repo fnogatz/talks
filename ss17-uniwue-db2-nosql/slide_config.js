22d2610ec3edd62f87a2fe4bcc9d0fee1ed66197
Falco Nogatz <fnogatz@gmail.com>
2017-07-19 13:48:54 +0200

    Add slides for DB2-Lecture on NoSQL

---
+++ ss17-uniwue-db2-nosql/slide_config.js
@@ -0,0 +1,39 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Post-Relational Databases',
+    subtitle: 'Beyond Tellerrand',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['default', 'uniwue-changes'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Falco Nogatz',
+    company: 'University of Würzburg',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@ulmerleben',
+    www: 'http://falco.io',
+    github: 'http://github.com/fnogatz'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
