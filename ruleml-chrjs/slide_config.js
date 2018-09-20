ad06450c197382a8741e021b70cd6903483925dc
Falco Nogatz <fnogatz@gmail.com>
2018-09-21 01:12:23 +0200

    Slides RuleML 2018

---
+++ ruleml-chrjs/slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'CHR.js: A CHR Implementation in JavaScript',
+    subtitle: '',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '6/x/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/uulm.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['default', 'uniwue-changes'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Falco Nogatz',
+    company: 'Lehrstuhl für Informatik I',
+    twitter: '@ulmerleben',
+    www: 'http://uni-wuerzburg.de/?nogatz',
+    github: 'http://github.com/fnogatz'
+  }]
+};
+
